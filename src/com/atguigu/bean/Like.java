26fb4d9c5b5dc911a7aebc1080978cec787d4252
ming37 <1216464893@qq.com>
2020-12-07 17:17:45 +0800

    xxxxxx

--- src/com/atguigu/bean/Like.java
+++ src/com/atguigu/bean/Like.java
@@ -4,6 +4,6 @@ public class Like {
 	public static void main(String[] args) {
 		System.out.println("11113333");
 		System.out.println("22223333");
-		System.out.println("6666");
+		System.out.println("8888");
 	}
 }
